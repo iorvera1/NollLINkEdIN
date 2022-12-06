d082971a5195460fc0ae1c422e14e44370845aa8
iorvera1 <109472075+iorvera1@users.noreply.github.com>
2022-12-05 17:05:45 -0800

    Add files via upload

---
+++ java_GUI.java
@@ -0,0 +1,34 @@
+package NollLINkEdIN;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+
+public class java_GUI {
+	
+	public static void main(String[] args) {
+		//JFrame = a GUI Window to add components
+		
+	/*	JFrame frame = new JFrame();//creates a frame
+		frame.setTitle("Title Goes here");//sets Title of frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Code for exit application
+		frame.setResizable(false);
+		frame.setSize(420, 420);//sets the x-dimensions, and y-diemensions of frame
+		frame.setVisible(true);//makes frame visible
+		
+		ImageIcon Image = new ImageIcon("Wallpaper_Test.png");//creates an Image icon
+		frame.setIconImage(Image.getImage());//changes icon of frame
+		frame.getContentPane().setBackground(new Color(200,200,244));//change color of backgrounds with hex codes i.e (0x000000)
+		*/ 
+		
+		//new JFrame_Test(); if i dont want to give it a name
+		
+		//JFrame_Test Test = new JFrame_Test();//if i want to give it a name
+		
+		new JLabel();
+	}
+
+}
